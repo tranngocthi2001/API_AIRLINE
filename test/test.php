aeee2309c4b7761f2fee69971de0e81405c9a205
Trần Văn Ngọc Thi <115824471+tranngocthi2001@users.noreply.github.com>
2024-03-03 03:55:47 +0700

    Update test.php Thêm command
    đọc command dòng 3-5

--- test/test.php
+++ test/test.php
@@ -1,9 +1,9 @@
 <?php
 
-$apiKey = 'vKnqLeZVR6k1AOHQzwrPP4cAxWVRTJLR';
-$apiSecret = 'ukdzzbePHfevXXmc';
-$accessTokenEndpoint = 'https://test.api.amadeus.com/v1/security/oauth2/token';
-$jsonResourceEndpoint = 'https://test.api.amadeus.com/v1/airline/destinations?airlineCode=BA';
+$apiKey = 'vKnqLeZVR6k1AOHQzwrPP4cAxWVRTJLR';//Đây là key a đăng ký trên web thầy cho.
+$apiSecret = 'ukdzzbePHfevXXmc';//Đây là key a đăng ký trên web thầy cho.
+$accessTokenEndpoint = 'https://test.api.amadeus.com/v1/security/oauth2/token'; //accsess token có thể sẽ có hạn sử dụng e có thể lên postman lấy, k đc ibx a chỉ
+$jsonResourceEndpoint = 'https://test.api.amadeus.com/v1/airline/destinations?airlineCode=BA'; //địa chỉ lấy dữ liệu json
 
 // Gửi yêu cầu để lấy access token
 $curl = curl_init();
